705cbf398e7de7aaba47941c87f41bd63fa9ab33
Maewan <vinc010@hotmail.com>
2020-06-03 11:02:17 +0200

    Test

--- exercices/Session 2/04/TestString.java
+++ exercices/Session 2/04/TestString.java
@@ -1,5 +1,6 @@
 public class TestString {
     public static String replaceName(String texte, String prenom, String nom){
+      System.out.println("Toto");
       return texte.replace("PRENOM", prenom).replace("NOM", nom);
     }
 
