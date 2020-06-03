3f6a6071326de151edd35f947d084b29d31e6626
Maewan <vinc010@hotmail.com>
2020-06-03 13:07:14 +0200

    Session 3 - 01

--- exercices/Session 3/01/Color.java
+++ exercices/Session 3/01/Color.java
@@ -1,9 +1,14 @@
 public class Color {
 
+  int red, green, blue;
+
   public Color(int R, int G, int B) {
+    this.red = R;
+    this.green = G;
+    this.blue = B;
   }
 
   public String toString() {
-    return null;
+    return "rgb("+red+","+green+","+blue+")";
   }
 }
